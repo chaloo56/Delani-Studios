1adf62573ca73266117fb94da342dfc614e8e1f9
chaloo56 <chaloo56@gmail.com>
2019-06-10 04:27:03 +0300

    added some javascript effect to what we do page

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(){
+    $(".road").hide();
+  $(".contain-f").hover(function(){
+      $(this).find(".road").toggle(400);
+  });
+  
+  });
+  $(".click0").click(function(){
+      $(".hide0").slideToggle(1000);
+      $(".show0").slideToggle(1200);
+    });
+  
+    $(".click1").click(function(){
+      $(".hide1").slideToggle(1000);
+      $(".show1").slideToggle(1200);
+    });
+  
+    $(".click2").click(function(){
+      $(".hide2").slideToggle(1000);
+      $(".show2").slideToggle(1200);
+    });
+  
+    $("button").click(function(party) {
+         var client = document.getElementById('nameDetail').value;
+         alert('Thanks ' + ' we have received your message successful. ' + '!');
+         party.preventDefault();
+     });
+    
+     $("button").on('click', function(){
+       $('form').each(function(){
+         this.reset();
+       });
+   });
